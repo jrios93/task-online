f82382e12aa8c7c0ae84a87877f321680cd16470
jrios93 <jcb.rios93@gmail.com>
2023-11-28 12:12:41 -0500

    Modificando aspecto y agregando algunas funcionalidades

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,36 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 
 import { Tasks } from './components/Tasks'
 import { useSEO } from './hooks/useSEO'
 import { useTaskList } from './hooks/useTaskList'
+import { FaRegCircleCheck } from 'react-icons/fa6'
 
 const link = ''
 
 const App = () => {
   const { taskList, handleAddItem, removeTask, checkboxChange } = useTaskList()
+  const [hourNow, setHourNow] = useState(new Date())
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setHourNow(new Date())
+    }, 1000)
+
+    return () => clearInterval(intervalId)
+  }, [])
+
+  function hour() {
+    const hour = hourNow.getHours()
+    const minute = hourNow.getMinutes()
+    const ampm = hour >= 12 ? 'PM' : 'AM'
+    const formattedHours = hour % 12 || 12
+
+    return `${formattedHours}:${minute < 10 ? '0' : ''}${minute} ${ampm}`
+  }
+
+  const hourtimeReal = hour()
+
   useSEO({
     title: `[${taskList.length} Tareas pendientes]`,
     description: 'Añadir y eliminar elementos de una lista',
@@ -32,38 +55,51 @@ const App = () => {
   const handleCheckboxChange = id => {
     checkboxChange(id)
   }
+
+  const completedTasks = taskList.filter(item => item.done).length
   return (
-    <>
-      <h1>Todo List</h1>
-      <form className="form-input" onSubmit={handleSubmit}>
-        <label style={{ width: '100%' }}>
-          <input
-            name="item"
-            type="text"
-            style={{
-              padding: '1.5em 2em',
-              width: '100%',
-              position: 'relative',
-            }}
-          />
-        </label>
-        <button style={{ position: 'absolute', marginLeft: '40%' }}>
-          Agregar
-        </button>
-      </form>
-      <ul>
-        {taskList.map(item => {
-          return (
-            <Tasks
-              {...item}
-              key={item.id}
-              handleCheckboxChange={() => handleCheckboxChange(item.id)}
-              handleDeleteTask={() => handleDeleteTask(item.id)}
-            />
-          )
-        })}
-      </ul>
-    </>
+    <div className=" h-auto w-screen p-16 bg-slate-950 text-slate-50 font-roboto">
+      <div className="flex justify-between">
+        <div className=" p-6 flex flex-col gap-20">
+          <p className="text-3xl flex gap-2 items-center">
+            <FaRegCircleCheck /> TodoApp
+          </p>
+          <p className="text-8xl font-orbitron">{hourtimeReal}</p>
+        </div>
+        <div className="p-6 flex flex-col gap-2 border-slate-500 border-2 rounded-lg h-full">
+          <div className="flex justify-between mb-6 ">
+            <p className="text-xl">Todo List</p>
+            <p className="border rounded-lg px-2 ">{`${completedTasks}/${taskList.length}`}</p>
+          </div>
+          <p className="text-3xl mb-6">Hola Diego 👋</p>
+          <ul className="flex flex-col gap-6 w-full mb-6">
+            {taskList.map(item => {
+              return (
+                <Tasks
+                  {...item}
+                  key={item.id}
+                  handleCheckboxChange={() => handleCheckboxChange(item.id)}
+                  handleDeleteTask={() => handleDeleteTask(item.id)}
+                />
+              )
+            })}
+          </ul>
+          <form className="form-input" onSubmit={handleSubmit}>
+            <label style={{ width: '100%' }}>
+              <input
+                name="item"
+                type="text"
+                placeholder="¿Que hay que hacer?"
+                className="w-full rounded-xl bg-transparent border-gray-500 border p-3 relative"
+              />
+              <button className=" absolute border rounded-xl p-2 -ml-20 mt-1 bg-gray-500">
+                Agregar
+              </button>
+            </label>
+          </form>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,3 +1,5 @@
+import { BsTrash3 } from 'react-icons/bs'
+
 export function Tasks({
   id,
   done,
@@ -8,29 +10,35 @@ export function Tasks({
   handleDeleteTask,
 }) {
   return (
-    <li className="form-list">
-      <input
-        type="checkbox"
-        checked={done}
-        onChange={() => handleCheckboxChange(id)}
-      />
-      <span
-        style={
-          done == true
-            ? { textDecoration: 'line-through' }
-            : { textDecoration: 'none' }
-        }
-      >
-        {text}
-      </span>
-      {link.length != link ? (
-        <a href={link} target="_blank" rel="noreferrer">
-          Link
-        </a>
-      ) : (
-        ''
-      )}
-      <button onClick={() => handleDeleteTask(id)}>X</button>
-    </li>
+    <div className="flex justify-between items-center gap-12">
+      <li className="border px-4 py-2 border-gray-700 rounded-lg w-full flex gap-2">
+        <input
+          type="checkbox"
+          checked={done}
+          onChange={() => handleCheckboxChange(id)}
+          className="border rounded-sm"
+        />
+        <span
+          style={
+            done == true
+              ? { textDecoration: 'line-through' }
+              : { textDecoration: 'none' }
+          }
+          className="text-lg"
+        >
+          {text}
+        </span>
+        {link.length != link ? (
+          <a href={link} target="_blank" rel="noreferrer">
+            Link
+          </a>
+        ) : (
+          ''
+        )}
+      </li>
+      <button onClick={() => handleDeleteTask(id)}>
+        <BsTrash3 className="text-lg" />
+      </button>
+    </div>
   )
 }
